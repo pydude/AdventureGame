15335376182ae82b60a20668fdf253aa7657ac69
pydude <69764029+pydude@users.noreply.github.com>
2020-08-16 12:27:02 -0500

    First upload

---
+++ Ryan:Alex,AdventureGame.py
@@ -0,0 +1,64 @@
+import random
+
+player = 150
+knight = 50
+knightgroup = 150
+royalguard = 100
+bigboss = 350
+smallboss = 275
+tenbossgroup = 400
+
+def checkPlayerHealth():
+    if player < 1:
+        print("You died.")
+        exit()
+
+def hardAttack():
+    global enemy
+    attack = random.randint(1, 17)
+    print("Player attacks!")
+    print("Attack = ", attack, "Points")
+    enemy = enemy - attack
+    print("Enemy Health:", enemy)
+
+def conserveAttack():
+    global enemy
+    attack = random.randint(7, 9)
+    print("Player attacks!")
+    print("Attack = ", attack, "Points")
+    enemy = enemy - attack
+    print("Enemy Health:", enemy)
+    
+def bigbossAttack():
+    global player
+    attack = random.randint(9, 21)
+    print("BigBoss attacks!")
+    print("Attack = ", attack, "Points")
+    player = player - attack
+    print("Player Health:", player)
+
+def smallbossAttack():
+    global player
+    attack = random.randint(8, 19)
+    print("SmallBoss attacks!")
+    print("Attack = ", attack, "Points")
+    player = player - attack
+    print("Player Health:", player)
+
+def tenbossgroupAttack():
+    global player
+    attack = random.randint(5, 19)
+    print("10BossGroup attacks!")
+    print("Attack = ", attack, "Points")
+    player = player - attack
+    print("Player Health:", player)
+
+print("You wake up in a room with a single door. Do you go through or fall back asleep?")
+print("You open the door and are in front of a castle. The guards appear mad at you. Fight or run?")
+
+
+print()
+bigbossAttack()
+smallbossAttack()
+tenbossgroupAttack()
+print()
